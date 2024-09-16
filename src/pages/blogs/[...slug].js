8aeb6d5b1e16ea5397604e7669d1f63f21ab99c0
Catbusp <web.developer@infinityadvt.com>
2024-09-16 16:56:15 +0530

    Create [...slug].js

---
+++ src/pages/blogs/[...slug].js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Server-side function to handle the request and prevent a 404
+export async function getServerSideProps(context) {
+  const { slug } = context.params;
+  
+  // Here you can handle different logic for the URL, but we return the slug
+  return {
+    props: {
+      slug, // Passing the slug as a prop to the page
+    },
+  };
+}
+
+// The default React component to render
+export default function CustomSlugPage({ slug }) {
+  return (
+    <></>
+  );
+}
